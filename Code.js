55999d63831ff88975dd1a2bc80a9bae576f3776
HRS295RICKY <71895974+HRS295RICKY@users.noreply.github.com>
2021-08-10 20:02:40 +0100

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "JAPAN";
+const accessCode2 = "CHINA";
+const accessCode3 = "INDIA";
+
+function clues() {
+
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    
+    text("N A J P A", 110,80)
+    text("Hint: land of rising sun !!", 110,100)
+    
+    text("H N A C I", 700,195)
+    text("Hint: most populated country !!", 700,215)
+    
+    text("N I D A I", 110,340)
+    text("Hint: country where in Taj Mahal is present !!", 110,360)
+
+}
